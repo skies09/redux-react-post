2d52271b4586955371c43af071a8875a96807cf2
Donna <skies09@hotmail.com>
2021-05-03 17:35:14 +0100

    initial commit

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,22 @@
+import { FETCH_POSTS, NEW_POSTS } from "../actions/types";
+
+const intitialState = {
+  items: [],
+  item: {},
+};
+export default function (state = intitialState, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+      return {
+        ...state,
+        items: action.payload,
+      };
+    case NEW_POSTS:
+      return {
+        ...state,
+        item: action.payload,
+      };
+    default:
+      return state;
+  }
+}
